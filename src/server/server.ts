fdfc4b40ca4dcc7eb4ef3f4f49358ec565eaa006
Josh Kimble <jkimble@aumni.fund>
2022-01-18 16:04:05 -0700

    remove logger

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,17 @@
 import express, { Request, Response } from 'express';
-import expressPinoLogger from 'express-pino-logger';
-
-import logger from './logger';
 
 const app = express();
 const port = 3000;
 
 export default () => {
-  app.use(expressPinoLogger({ logger }));
-  app.get('/', (_req: Request, res: Response) => {
-    res.send('Hello World!');
+  app.get('/health', (_req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'Ok',
+    });
   });
 
   app.listen(port, () => {
-    logger.info(`app listening at http://localhost:${port}`);
+    // eslint-disable-next-line no-console
+    console.info(`app listening at http://localhost:${port}`);
   });
 };
